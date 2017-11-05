4c57e874dede3f0757c3b53494db4d9cdd60545e
Anthony Eden <anthony@mediarealm.com.au>
2017-11-05 16:39:40 +1100

    Create the basic script to take the current text from ProPresenter and send it to one or more Resolume TextAreas.

---
+++ ProPresenter-Resolume.py
@@ -0,0 +1,139 @@
+""" ProPresenter & Resolume Link - Send ProPresenter text to Resolume """
+
+import os
+import json
+import time
+from ProPresenterStageDisplayClientComms import ProPresenterStageDisplayClientComms
+from pythonosc import osc_message_builder
+from pythonosc import udp_client
+
+__author__ = "Anthony Eden"
+__copyright__ = "Copyright 2017, Anthony Eden / Media Realm"
+__credits__ = ["Anthony Eden"]
+__license__ = "GPL"
+__version__ = "0.1"
+
+class ProPResolume():
+
+    # Store the threaded classes for ProPresenter and Resolume
+    ProPresenter = None
+    Resolume = None
+
+    # Config data for ProPresenter
+    ProP_IPAddress = None
+    ProP_IPPort = None
+    ProP_Password = None
+
+    # Config data for Resolume
+    Resolume_IPAddress = None
+    Resolume_IPPort = None
+    Resolume_TextBoxOSCPaths = []
+
+    # Do we need to attempt a reconnection?
+    tryReconnect = False
+    disconnectTime = 0
+
+    def __init__(self):
+        # Setup the application
+
+        # Get the config from JSON
+        try:
+            ConfigData_Filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), "config.json")
+            ConfigData_JSON = open(ConfigData_Filename).read()
+            ConfigData = json.loads(ConfigData_JSON)
+
+        except Exception as e:
+            print()
+            print("##############################################")
+            print("EXCEPTION: Cannot load and parse Config.JSON File: ")
+            print(e)
+            print("##############################################")
+            print()
+
+            exit()
+
+        try:
+            self.ProP_IPAddress = ConfigData['ProP_IPAddress']
+            self.ProP_IPPort = int(ConfigData['ProP_IPPort'])
+            self.ProP_Password = ConfigData['ProP_Password']
+
+            self.Resolume_IPAddress = ConfigData['Resolume_IPAddress']
+            self.Resolume_IPPort = int(ConfigData['Resolume_IPPort'])
+            self.Resolume_TextBoxOSCPaths = ConfigData['Resolume_TextBoxOSCPaths']
+
+        except Exception as e:
+            print()
+            print("##############################################")
+            print("EXCEPTION: Config file is missing a setting")
+            print(e)
+            print("##############################################")
+            print()
+
+            exit()
+
+        self.connectProP()
+        self.connectResolume()
+        self.reconnect_tick()
+
+    def connectProP(self):
+        # Connect to ProPresenter and setup the necessary callbacks
+        self.tryReconnect = False
+        self.disconnectTime = 0
+
+        self.ProPresenter = ProPresenterStageDisplayClientComms(self.ProP_IPAddress, self.ProP_IPPort, self.ProP_Password)
+        self.ProPresenter.addSubscription("CurrentSlide", self.updateSlideTextCurrent)
+        self.ProPresenter.addSubscription("Connected", self.connected)
+        self.ProPresenter.addSubscription("ConnectionFailed", self.connectFailed)
+        self.ProPresenter.addSubscription("Disconnected", self.disconnected)
+        self.ProPresenter.start()
+    
+    def connectResolume(self):
+        self.Resolume = udp_client.SimpleUDPClient(self.Resolume_IPAddress, self.Resolume_IPPort)
+
+    def connected(self, data):
+        print("ProPresenter Connected")
+
+    def connectFailed(self, error):
+        self.tryReconnect = True
+
+        if self.disconnectTime == 0:
+            self.disconnectTime = time.time()
+
+        print("ProPresenter Connect Failed", error)
+
+    def disconnected(self, error):
+        self.tryReconnect = True
+
+        if self.disconnectTime == 0:
+            self.disconnectTime = time.time()
+
+        print("ProPresenter Disconnected", error)
+
+    def reconnect_tick(self):
+        if self.tryReconnect and self.disconnectTime < time.time() - 5:
+            print("Attempting to reconnect to ProPresenter")
+            self.connectProP()
+
+        #self.labelCurrent.after(2000, self.reconnect_tick)
+
+    def updateSlideTextCurrent(self, data):
+        # Update the text label for the current slide
+        if data['text'] is not None:
+            self.resolumeSendText(data['text'].encode('utf-8'))
+        else:
+            self.resolumeSendText("\n")
+
+    def resolumeSendText(self, text):
+        if type(text) is bytes:
+            text = text.decode().strip(' \t\r\n\0')
+
+        for path in self.Resolume_TextBoxOSCPaths:
+            self.Resolume.send_message(path, text)
+
+    def close(self):
+        # Terminate the application
+        if self.ProPresenter is not None:
+            self.ProPresenter.stop()
+
+if __name__ == "__main__":
+    ProPResolume()
---
+++ ProPresenterStageDisplayClientComms.py
@@ -0,0 +1,182 @@
+"""ProPresenter Stage Display Client (Communication Class). An Open-Source Python Client for the ProPresenter6 Stage Display XML Feed."""
+
+import socket
+import errno
+import time
+import threading
+import xml.etree.ElementTree as ET
+
+__author__ = "Anthony Eden"
+__copyright__ = "Copyright 2017, Anthony Eden / Media Realm"
+__credits__ = ["Anthony Eden"]
+__license__ = "GPL"
+__version__ = "0.1"
+
+
+class ProPresenterStageDisplayClientComms(threading.Thread):
+    """This class handles all the communications with the ProPresenter Stage Display server."""
+
+    # The handle for the socket connection to ProPresenter
+    sock = None
+
+    # A list of data types to subscribe to (with callbacks)
+    dataSubscriptions = []
+
+    # Should we be shutting down this thread? Set via self.stop()
+    _stop = False
+    
+    # Store the password used to connect to the ProPresenter Stage Display socket
+    password = ""
+
+    def __init__(self, host, port, password):
+        """Setup the class"""
+
+        self.sock = None
+        self.host = host
+        self.port = port
+        self.password = password
+        self.dataSubscriptions = []
+
+        # Start the thread
+        threading.Thread.__init__(self)
+
+    def stop(self):
+        """Attempt to close this thread."""
+        self._stop = True
+
+    def run(self):
+        """Create a socket connection to the ProPresenter server"""
+
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        try:
+            self.sock.connect((self.host, self.port))
+        except:
+            # End the thread and call a callback
+            self.doCallback("ConnectionFailed", "")
+            self.stop()
+        else:
+            self.doCallback("Connected", "")
+            self.sock.setblocking(0)
+            self.loop()
+
+    def loop(self):
+        """A method that keeps going forever, and handles all the communication with the open ProPresenter socket."""
+
+        # Login to ProPresenter
+        self.sendCommand("<StageDisplayLogin>"+self.password+"</StageDisplayLogin>\r\n")
+
+        while True:
+            if self._stop is True:
+                # End the thread
+                self.sock.close()
+                break
+
+            # Try and receive data from ProPresenter
+            recvData = self.recvUntilNewline()
+
+            if recvData is False:
+                self.sock.close()
+                break
+
+            if recvData is not None:
+                self.processReceivedData(recvData)
+
+            # Lower this number to receive data quicker
+            time.sleep(0.2)
+
+    def recvUntilNewline(self):
+        """Receive data until we get to the end of a message."""
+        totalData = ""
+        inBlock = False
+
+        while True:
+            try:
+                thisData = self.sock.recv(1024).decode()
+
+                if thisData == "":
+                    # Connection closed
+                    self.sock.close()
+                    self.doCallback("Disconnected", "")
+                    return False
+
+                else:
+                    totalData += thisData
+
+            except socket.error as e:
+                if e.args[0] != errno.EWOULDBLOCK:
+                    self.sock.close()
+                    self.doCallback("Disconnected", e)
+                    return False
+
+            else:
+                # Check if we're in a data block
+                if totalData[:80] == '<StageDisplayData>':
+                    inBlock = True
+
+                # Check if the datablock is over
+                if "</StageDisplayData>" in totalData[-25:]:
+                    return totalData
+            
+                if self._stop is True:
+                    # End the thread
+                    self.sock.close()
+                    break
+
+    def processReceivedData(self, recvData):
+        """Process the received data from ProPresenter. Attempts to parse it and trigger the subscribed callback."""
+        
+        try:
+            tree = ET.fromstring(recvData)
+        except:
+            # This is an unexpected data type. Ignore it for now...
+            pass
+        else:
+        
+            for fields in tree.findall('Fields'):
+                for slideElement in fields.findall('Field'):
+                    if 'identifier' in slideElement.attrib:
+                        dataType = slideElement.attrib['identifier']
+                        
+                        # This dict will have all the data to return to our callbacks
+                        returnData = {}
+                        
+                        # Send the actual text
+                        returnData['text'] = slideElement.text
+                        
+                        # Send all the attributes
+                        for i, key in enumerate(slideElement.attrib):
+                            returnData[key] = slideElement.attrib[key]
+                        
+                        # Trigger a callback
+                        self.doCallback(dataType, returnData)
+
+    def sendCommand(self, msg):
+        """Buffer a command to send."""
+        self.sock.send(msg.encode() + "\r\n".encode())
+    
+    def addSubscription(self, subType, callbackObj, limit=False, filters={}):
+        """Add a subscription to the list of data subscriptions."""
+        self.dataSubscriptions.append({
+            "commandType": subType,
+            "callback": callbackObj,
+            "limit": limit
+        })
+
+    def doCallback(self, dataType, returnData):
+        # Loop over every subscription
+        for subI, subX in enumerate(self.dataSubscriptions):
+
+            # If the subscribed command type matches the message's command type
+            if dataType == subX['commandType']:
+
+                # Execute the callback!
+                subX['callback'](returnData)
+
+                # Check if we need to decrement the limit
+                if self.dataSubscriptions[subI]['limit'] is not False:
+                    self.dataSubscriptions[subI]['limit'] = self.dataSubscriptions[subI]['limit'] - 1
+
+                # Check if we need to remove this subscription
+                if self.dataSubscriptions[subI]['limit'] <= 0 and self.dataSubscriptions[subI]['limit'] is not False:
+                    self.dataSubscriptions.pop(subI)
